e656123a295173795c9a2d1b63133b6eaa45d57e
Jarrod Barnes <jbarnes850@gmail.com>
2024-12-06 15:23:06 -0500

    Fix: added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 import '@fontsource/space-grotesk/400.css';
@@ -10,5 +11,6 @@ import '@fontsource/jetbrains-mono/400.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
